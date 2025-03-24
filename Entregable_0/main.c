840e177db6dff1d0dbdbb2a93887d7a1f73447c9
enzogiandon <enzo7agustin@gmail.com>
2025-03-24 19:50:42 -0300

    Revert "ej f"
    This reverts commit c5fa5d7bf476a9f746e2cadd02e8b50551774127.

--- Entregable_0/main.c
+++ Entregable_0/main.c
@@ -21,39 +21,72 @@ int main()
 {
     printf("Entregable 0\nIngrese un número con decimal, que será validado posteriormente:\n");
 
-    char entrada[30];
+    char numero[30];
 
     // Chequeo validez de la entrada
-    if (scanf("%s",entrada) !=1)
+    if (scanf("%s",numero) !=1)
     {
         printf("Entrada inválida.");
         return 1;
     }
 
-    int parte_entera, parte_fraccionaria;
-    sscanf(entrada, "%d.%d", &parte_entera, &parte_fraccionaria);
 
+    int signo = 1;
+    int i=0;
 
-    //REVISAR RANGO
-
-    // Si el numero es menor a -2^(7) o mayor a 2^7
-
-    if ((parte_fraccionaria < ((-1) * pow(2,A))) || parte_entera > (pow(2,A))){
-            printf("Fuera de rango");
-            return 1;
+    // Chequeo signo
+    if (numero[0] == '-')
+    {
+        signo = -1;
+        i++;
     }
 
+    // Analizo la parte entera digito a digito
+    int parte_entera =0;
 
-    // Conversion a Q(A,B)
+    while(isdigit(numero[i]))
+    {
+        parte_entera = parte_entera *10 + (numero[i] - '0'); // Conversion de digito a numero
+        i++;
+    }
 
-    int16_t valor = ((parte_entera << (A+1)) | (parte_fraccionaria & 0xFF));
+    // Imprimo (prueba)
+    printf("Parte entera: %d\n",parte_entera);
 
-    printf("Hexadecimal: 0x%4x\n",valor);
 
-/*
-    printf("Binario: ");
+
+    // Analizo la parte fraccionaria digito a digito
+
+    int parte_fraccionaria = 0;
+    int mult_fraccionario = 1;
+
+    if ((numero[i] == '.') || (numero[i] == ','))
+    {
+        i++;
+        while(isdigit(numero[i]))
+        {
+            parte_fraccionaria = parte_fraccionaria *10 + (numero[i] - '0');
+            mult_fraccionario *= 10;
+            i++;
+        }
+    }
+    //Imprimo (prueba)
+
+    printf("Parte fraccionaria: %d\n",parte_fraccionaria);
+
+    // Conversion a Q(7,8)
+
+    // Hacemos el corrimiento de los bits para que la parte entera sea la mas significativa
+
+    int32_t escalaEntera = parte_entera << A; // corro d posiciones
+
+    int32_t escalaDecimal = (parte_fraccionaria << A) / mult_fraccionario;
+
+    int32_t valor = signo * (escalaEntera + escalaDecimal);
+
+    printf("Decimal: %d , Hexadecimal: 0x%x\nBinario: ",valor, (uint32_t) valor);
+
     convertToBinary(valor);
-*/
     return 0;
 }
 
