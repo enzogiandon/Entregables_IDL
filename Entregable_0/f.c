d47e2c69b284e5e2b15a80f1e98fb1a9b5b0fc71
enzogiandon <enzo7agustin@gmail.com>
2025-03-25 20:28:16 -0300

    f terminado

---
+++ Entregable_0/f.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define A 7
+#define B 8
+
+void convertToBinary16(unsigned a,int i)
+{
+    i++;
+    if (i<16) // solo para 16 bits
+    {
+        /* step 1 */
+        if (a > 1) convertToBinary16(a / 2,i);
+        /* step 2 */
+        printf("%d", a % 2);
+    }
+}
+int main()
+{
+    printf("Entregable 0\n Ingrese un número con decimal, que será validado posteriormente:\n");
+    char numero[30];
+    // Chequeo validez de la entrada
+    if (scanf("%s",numero) !=1)
+    {
+        printf("Entrada inválida.");
+        return 1;
+    }
+    int signo = 1;
+    int i=0;
+    // Chequeo signo
+    if (numero[0] == '-')
+    {
+        signo = -1;
+        i++;
+    }
+
+    // Analizo la parte entera digito a digito
+    int parte_entera =0;
+
+    while(isdigit(numero[i]))
+    {
+        //Conversión de dígito a número
+        parte_entera = parte_entera * 10 + (numero[i] - '0');
+        i++;
+    }
+
+
+    // Analizo la parte fraccionaria digito a digito
+    int parte_fraccionaria = 0;
+    int mult_fraccionario = 1;
+
+    if ((numero[i] == '.') || (numero[i] == ','))
+    {
+        i++;
+        while(isdigit(numero[i]))
+        {
+            parte_fraccionaria = parte_fraccionaria *10 + (numero[i] - '0');
+            mult_fraccionario *= 10;
+            i++;
+        }
+    }
+
+    // REVISAR VALORES DE RANGO (No deberia fijarse mientras va extrayendo los digitos?)
+    if ((signo * parte_entera < (-128) || signo * parte_entera > 127))
+    {
+        printf("Fuera de rango");
+        return 1;
+    }
+
+    // Conversion a Q(7,8)
+    // Hacemos el corrimiento de los bits para que la parte entera sea la mas significativa
+    int16_t escalaEntera = (parte_entera << B); // corro B posiciones
+    int16_t escalaDecimal = (parte_fraccionaria << B) / mult_fraccionario; // corro B posiciones / 10^ n digitos
+    int16_t valor = signo * (escalaEntera + escalaDecimal);
+    printf("Hexadecimal: 0x%04X\n", (uint16_t) valor);
+
+    printf("Binario: ");
+    if (signo ^ 1)
+        printf("%d",1);
+    else
+        printf("%d",0);
+
+    convertToBinary16(valor,0);
+
+    return 0;
+}
